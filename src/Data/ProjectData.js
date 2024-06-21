18e7d2ed9367c993f1f94f85e70a1c4db72f0382
dheeraj123495 <dheerajsp0700@gmail.com>
2024-06-22 00:49:11 +0530

    Skills and Resume Update

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -7,23 +7,40 @@ export const ProjectData = [
     skills: "React.js, Express.js,Redux.js, MongoDB,  Tailwind CSS.",
     description:
       "Developed a MERN social media application enabling users to connect, share, and engage through posts, likes, and comments. ",
+    link: "https://github.com/dheeraj123495/WeConnect",
+    live: "https://we-connect-one.vercel.app/",
   },
   {
     id: 2,
+    image:
+      "https://img.freepik.com/free-vector/flat-design-monsoon-season-clouds-illustration_23-2149424294.jpg?t=st=1717059195~exp=1717062795~hmac=5d906aedcc0c2624486fa3d1db7635ad25436a452c5c925ec1c168c73a11520f&w=1060",
+    title: "Weather App",
+    skills: "React.js, CSS, APIs.",
+    description:
+      "Engineered a React-based web application that delivered real-time temperature data for 50+ global locations. ",
+    link: "https://github.com/dheeraj123495/weatherapp",
+    live: "https://weatherapp-puce-pi.vercel.app/",
+  },
+  {
+    id: 3,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtM4qHf43n71KbpWEV67inzg_aN2945f8-RAvboOTm6heYYaDYPFVq7kZmMxcZ-si1lnk&usqp=CAU",
+    title: "Employee Management System",
+    skills: "C#, ASP.NET, CSS, JS, SQL, MySQL WorkBench.",
+    description:
+      "This web application demonstrates CRUD operations with multi-user, role-based access control.",
+    link: "https://github.com/dheeraj123495/Employee_Managment_System",
+    live: "https://github.com/dheeraj123495/Employee_Managment_System",
+  },
+  {
+    id: 4,
     image:
       "https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcm00NjUtYmctMDUta3cwZWhmenkuanBn.jpg",
     title: "Todo App",
     skills: "React.js, Tailwind CSS.",
     description:
       "Built a React Todo App enabling users to manage tasks and track progress with a clean and responsive design.",
-  },
-  {
-    id: 3,
-    image:
-      "https://img.freepik.com/free-vector/flat-design-monsoon-season-clouds-illustration_23-2149424294.jpg?t=st=1717059195~exp=1717062795~hmac=5d906aedcc0c2624486fa3d1db7635ad25436a452c5c925ec1c168c73a11520f&w=1060",
-    title: "Weather App",
-    skills: "React.js, CSS, APIs",
-    description:
-      "Engineered a React-based web application that delivered real-time temperature data for 50+ global locations. ",
+    link: "#",
+    live: "#",
   },
 ];
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -14,12 +14,12 @@ const Projects = ({ ProjectData }) => {
             return (
               <div
                 key={project.id}
-                className="flex bg-slate-100 rounded-md md:w-3/12 md:min-h-[400px] flex-col hover:shadow-lg hover:cursor-pointer transition-all duration-300"
+                className="flex bg-slate-100 rounded-md md:w-3/12 md:min-h-[480px] flex-col hover:shadow-lg hover:cursor-pointer transition-all duration-300"
               >
                 <img
                   src={project.image}
                   alt={project.name}
-                  className="max-h-[200px] w-screen rounded-lg p-2"
+                  className="max-h-[200px] w-screen rounded-lg p-1"
                 />
                 <div className="flex flex-col text-center justify-center md:w-10/12 md:mx-auto">
                   <h5 className="text-richblack-600 font-poppins font-semibold tracking-widest text-3xl md:text-[1.3rem]">
@@ -29,8 +29,21 @@ const Projects = ({ ProjectData }) => {
                     {project.description}
                   </p>
                   <p className="font-lora md:p-2 p-3 font-normal text-gray-500 md:text-1xl text-left">
-                    <span className="font-bold">Skills </span> : {project.skills}
+                    <span className="font-bold">Skills </span> :{" "}
+                    {project.skills}
                   </p>
+                  <div className="flex justify-around flex-wrap mt-[px]">
+                    <button className="bg-richblack-900 hover:bg-white border-2 hover:border-richblack-900 hover:text-black transition-all duration-300 font-thick text-white px-4 py-2 tracking-widest mb-3">
+                      <a href={project.link} target="_blank" rel="noreferrer">
+                        GitHub
+                      </a>
+                    </button>
+                    <button className="bg-richblack-900 hover:bg-white border-2 hover:border-richblack-900 hover:text-black transition-all duration-300 font-thick text-white px-4 py-2 tracking-widest mb-3">
+                      <a href={project.live} target="_blank" rel="noreferrer">
+                        Live
+                      </a>
+                    </button>
+                  </div>
                 </div>
               </div>
             );
